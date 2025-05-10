3754ba2a2c3b06f34cb3c478a5f850efa3e1666c
Amelia Johnson <111928731+amelia-online@users.noreply.github.com>
2025-05-10 16:04:51 -0600

    add googletest submodule

--- dynarray.c
+++ dynarray.c
@@ -1,21 +1,91 @@
 #include "dynarray.h"
 #include <stdlib.h>
-#include <stdio.h>
 
-GENERIC_VEC(int, IntVec)
-
-IntVec IntVecMake()
+BitVec BitVecMake()
 {
-  return (IntVec)
-  {
-    .size = 0,
-    .array = malloc(sizeof(int)*8)
-  };
+  return (BitVec)
+    {
+      .array = malloc(sizeof(unsigned char)),
+      .cap = 8,
+      .size = 0,
+      .top = 0,
+      .byte = 0;
+    };
 }
 
-int main()
+void BitVecFree(BitVec *v)
 {
-  IntVec vec = IntVecMake();
-  printf("%lld\n", vec.size);
-  return 0;
+  free(v->array);
+}
+
+void BitVecResize(BitVec *v)
+{
+  v->array = realloc(v->array, v->cap/8 * 2);
+  v->cap *= 2;
+}
+
+void BitVecPush(BitVec *v, bool bit)
+{
+  if (v->size + 1 >= v->cap)
+    BitVecResize(v);
+
+  if (!bit)
+    {
+      v->top++;
+      v->size++;
+      return;
+    }
+  
+  int idx = v->top / 8;
+  int bitIdx = v->top % 8;
+  v->array[idx] = v->array[idx] | (128 >> bitIdx);
+  v->top++;
+  v->size++;
+}
+
+bool *BitVecPop(BitVec *v)
+{
+  if (v->size == 0)
+    return NULL;
+
+  bool *res = malloc(sizeof(bool));
+
+  int byte = v->top / 8;
+  int idx = v->top % 8;
+
+  *res = v->array[byte] & (128 >> idx);
+
+  v->size--;
+  v->top--;
+  
+  return res;
+}
+
+bool *BitVecPeek(const BitVec *v)
+{
+  if (v->size == 0)
+    return NULL;
+
+  bool *res = malloc(sizeof(bool));
+
+  int byte = v->top / 8;
+  int idx = v->top % 8;
+
+  *res = v->array[byte] & (128 >> idx);
+  
+  return res;
+}
+
+bool *BitVecGet(const BitVec *v, int idx)
+{
+  if (idx < 0 || idx > v->size)
+    return NULL;
+
+  bool *res = malloc(sizeof(bool));
+  int byte = idx / 8;
+  int bitIdx = idx % 8;
+
+  *res = v->array[byte] & (128 >> bitIdx);
+  
+  return res;
 }
--- dynarray.h
+++ dynarray.h
@@ -1,48 +1,102 @@
 #ifndef ESSENTIALS_VEC_H
 #define ESSENTIALS_VEC_H
 
+#include <stdlib.h>
+#include <stdbool.h>
+
 #define DEF_GENERIC_VEC(TYPE, NAME) \
-  typedef struct { \
-    int64_t size; \
-    TYPE *array; \
-    int64_t top; \
-    int64_t cap; \
+  typedef struct {		    \
+    int64_t size;		    \
+    TYPE *array;		    \
+    int64_t top;		    \
+    int64_t cap;		    \
   } NAME;
-#define GENERIC_VEC_HEADERS(TYPE, NAME) \
-  NAME NAME##Make(); \
-  void NAME##Free(NAME *); \
+#define GENERIC_VEC_HEADERS(TYPE, NAME)			\
+  NAME NAME##Make();					\
+  void NAME##Free(NAME *);				\
   void NAME##Push(NAME *, TYPE);			\
-  TYPE NAME##Pop(NAME *); \
-  TYPE NAME##Peek(const NAME *); \
-  TYPE NAME##Get(const NAME *, int);		\
-  void NAME##Remove(NAME *, int);		\
+  TYPE *NAME##Pop(NAME *);				\
+  TYPE *NAME##Peek(const NAME *);			\
+  TYPE *NAME##Get(const NAME *, int);			\
+  void NAME##Remove(NAME *, int);			\
   void NAME##Resize(NAME *);
 
-#define IMPL_GENERIC_VEC(TYPE, NAME) \
-  NAME NAME##Make() { \
-    return (NAME) { \
-      .size = 0, \
-      .top = 0; \
-      .array = malloc(sizeof(TYPE)*16); \
-    }; \
-  } \
-  void NAME##Free(NAME *x) { \
-    free(x->array); \
-  } \
-  void NAME##Push(NAME *v, TYPE t) {		\
-    if (v->size + 1 >= cap) \
-       NAME##Resize(v); \
-    v->array[v->top] = t; \
-    v->top++; \
-  } \
-  TYPE NAME##Pop(NAME *v) { \
-    
-  
-  
+#define IMPL_GENERIC_VEC(TYPE, NAME)				\
+  NAME NAME##Make() {						\
+    return (NAME) {						\
+      .size = 0,						\
+      .top = 0;							\
+      .array = malloc(sizeof(TYPE)*16);				\
+    };								\
+  }								\
+  void NAME##Free(NAME *x) {					\
+    free(x->array);						\
+  }								\
+  void NAME##Push(NAME *v, TYPE t) {				\
+    if (v->size + 1 >= cap)					\
+      NAME##Resize(v);						\
+    v->array[v->top] = t;					\
+    v->top++;							\
+    v->size++;							\
+  }								\
+  TYPE *NAME##Pop(NAME *v) {					\
+    if (v->size == 0)						\
+      return NULL;						\
+    TYPE *res = malloc(sizeof(TYPE));				\
+    *res = v->array[v->top];					\
+    v->top -= 1;						\
+    v->size -= 1;						\
+    return res;							\
+  }								\
+  TYPE *NAME##Peek(NAME *v) {					\
+    if (v->size == 0)						\
+      return NULL;						\
+    TYPE *res = malloc(sizeof(TYPE));				\
+    *res = v->array[v->top];					\
+    return res;							\
+  }								\
+  TYPE *NAME##Get(const NAME *v, int idx) {			\
+  if (idx < 0 || idx > v->size-1)				\
+    return NULL;						\
+  TYPE *res = malloc(sizeof(TYPE));				\
+  *res = v->array[idx];						\
+  return res;							\
+  }								\
+   void NAME##Remove(NAME *v, int idx) {			\
+     if (idx < 0 || idx > v->size-1)				\
+       return;							\
+     for (int i = idx; i < v->size-1; idx++) {			\
+       v->array[i] = v->array[i+1];				\
+     }								\
+     v->top--;							\
+     v->size--;							\
+   }								\
+   void NAME##Resize(NAME *v) {					\
+     v->array = realloc(v->array, v->cap * 2 * sizeof(TYPE));	\
+     v->cap *= 2;						\
+   }						
+     
 
-#define GENERIC_VEC(TYPE, NAME) \
-  DEF_GENERIC_VEC(TYPE, NAME) \
+#define GENERIC_VEC(TYPE, NAME)	    \
+  DEF_GENERIC_VEC(TYPE, NAME)	    \
     GENERIC_VEC_HEADERS(TYPE, NAME) \
-    IMPL_GENERIC_VEC(TYPE, NAME) 
+    IMPL_GENERIC_VEC(TYPE, NAME)
+
+typedef struct {
+  unsigned char *array;
+  int64_t size;
+  int64_t cap;
+  int64_t top;
+  int64_t byte;
+} BitVec;
+
+BitVec BitVecMake();
+void BitVecFree(BitVec *);
+void BitVecPush(BitVec *, bool);
+bool *BitVecPop(BitVec *);
+bool *BitVecPeek(const BitVec *);
+bool *BitVecGet(const BitVec *, int);
+void BitVecRemove(BitVec *, int);
+void BitVecResize(BitVec *);
 
 #endif // ESSENTIALS_VEC_H
